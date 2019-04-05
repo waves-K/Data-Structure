17cfbb84450fbf722a169293dc2b51649831f331
waves-K <1505906381@qq.com>
2019-04-05 22:24:22 +0800

    单链表 Remove & RemoveAll

--- practice/SList.c
+++ practice/SList.c
@@ -62,7 +62,7 @@ void SListPushBack(SList* slist, DataType data){
   // 遍历链表，找到最后一个结点
   else{
     Node* cur = slist->first;
-    while(cur != NULL){
+    while(cur->next != NULL){
       cur = cur->next;
     }
     cur->next = node;
@@ -73,7 +73,7 @@ void SListPushBack(SList* slist, DataType data){
 // 头删
 void SListPopFront(SList* slist){
   // 头删 必须保证链表中有元素
-  assert(slist->first->next != NULL);
+  assert(slist->first != NULL);
 
   // 删除后头结点要能找到剩下的链表
   Node* second = slist->first->next;
@@ -85,17 +85,32 @@ void SListPopFront(SList* slist){
 // 尾删
 void SListPopBack(SList* slist){
   // 1. 保证链表中有元素
-  assert(slist->first->next != NULL);
+  assert(slist->first != NULL);
 
   // 2. 找到链表倒数第二个结点
   Node* cur = slist->first;
-  while(cur->next != NULL){
+  while(cur->next->next != NULL){
     cur = cur->next;
   }
   free(cur->next);
-  cur = NULL; 
+  cur->next = NULL; 
 }
 
+// 查找值为 data 的结点
+Node* SListFind(SList* slist, DataType data){
+  assert(slist);
+
+  Node* cur = slist->first;
+  while(cur->value != data){
+    cur = cur->next;
+  }
+  if(cur != NULL){
+    return cur;
+  }
+  else{
+    return NULL;
+  }
+}
 
 // 在 pos 结点之后插入
 void SListInsertAfter(SList* slist, Node* pos, DataType data ){
@@ -104,7 +119,6 @@ void SListInsertAfter(SList* slist, Node* pos, DataType data ){
   (void)slist;
   Node* node = (Node*)malloc(sizeof(Node));
   node->value = data;
-  node->next = NULL;
 
   node->next = pos->next;
   pos->next = node;
@@ -121,6 +135,68 @@ void SListEraseAfter(SList* slist, Node* pos){
   Node* cur = pos->next->next;
   free(pos->next);
   pos->next = cur;
+}
+
+// 删除链表中第一个值为 data 的结点
+void SListRemove(SList* slist, DataType data){
+  assert(slist);
+
+  // 链表中没有结点
+  if(slist->first == NULL){
+    return ;
+  }
+
+  // 链表中有结点
+  // 判断链表中第一个结点的值是不是要删除 结点的值
+  // 如果是，头删，不是则遍历链表，查找结点值为 data 的结点
+  if(slist->first->value == data){
+    Node* second = slist->first->next;
+    free(slist->first);
+    slist->first = second;
+  }
+
+  Node* prev = slist->first;
+  Node* cur = prev->next;
+  while(cur->value != data){
+    cur = cur->next; 
+    prev = prev->next;
+  }
+  if(cur != NULL){
+    Node* next = cur->next;
+    free(cur);
+    prev->next = next;
+  }
+  else{
+    return;
+  }
+}
+
+
+// 删除链表中所有值等于 data 的结点
+void SListRemoveAll(SList* slist, DataType data){
+  assert(slist);
+
+  if(slist->first == NULL){
+    return ;
+  }
+
+  Node* cur = slist->first;
+  while(cur->next != NULL){
+    if(cur->next->value == data){
+      Node* next = cur->next;
+      cur->next = cur->next->next;
+      free(next);
+    }
+    else{
+      cur = cur->next;
+    }
+  }
+
+  if(slist->first->value == data){
+    Node* first = slist->first;
+    slist->first = slist->first->next;
+    free(first);
+  }
 }
 
 // 链表销毁
@@ -141,6 +217,7 @@ void PrintSList(SList* slist){
     printf("%d ", cur->value);
     cur = cur->next;
   }
+  printf("\n");
 }
 
 void test(){
@@ -159,10 +236,44 @@ void test(){
 
   SListPopFront(&slist);
   SListPopFront(&slist);
+  SListPopFront(&slist);
   PrintSList(&slist);
 
   SListPopBack(&slist);
   SListPopBack(&slist);
+  PrintSList(&slist);
+
+  Node* pos = SListFind(&slist, 4);
+
+  SListInsertAfter(&slist, pos, 8);
+  SListInsertAfter(&slist, pos, 7);
+  SListInsertAfter(&slist, pos, 6);
+  SListInsertAfter(&slist, pos, 5);
+  PrintSList(&slist);
+
+  Node* Pos = SListFind(&slist, 6);
+  SListEraseAfter(&slist, Pos);
+  SListEraseAfter(&slist, Pos);
+  PrintSList(&slist);
+
+  SListInsertAfter(&slist, pos, 8);
+  SListInsertAfter(&slist, pos, 7);
+  SListInsertAfter(&slist, pos, 6);
+  PrintSList(&slist);
+
+  SListRemove(&slist, 6);
+  PrintSList(&slist);
+
+
+  SListInsertAfter(&slist, pos, 6);
+  SListInsertAfter(&slist, pos, 6);
+  SListInsertAfter(&slist, pos, 6);
+  SListPushBack(&slist, 5);
+  SListPushBack(&slist, 6);
+  SListPushFront(&slist, 6);
+  PrintSList(&slist);
+
+  SListRemoveAll(&slist, 6);
   PrintSList(&slist);
 
   SListDestroy(&slist);
